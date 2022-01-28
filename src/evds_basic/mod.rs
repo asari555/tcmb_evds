be8524efa124e75814cd51f9700ad470a3d22b21
asari555 <asari571@gmail.com>
2022-01-28 18:29:24 +0300

    Editted comments.

--- src/evds_basic/mod.rs
+++ src/evds_basic/mod.rs
@@ -12,57 +12,57 @@ use crate::traits::MakingUrlFormat;
 ///
 /// [`Date`](struct@crate::date::Date) and [`DateRange`](struct@crate::date::DateRange) are both acceptable
 /// for this function.
-/// 
+///
 /// *User is responsible to supply correct and valid* **data series** *argument for this function.*
 ///
 /// # Error
 ///
 /// This function returns an error if some of given parameters is empty, the internet connection is poor or/and
-/// the format of the request is invalid or/and incorrect. 
+/// the format of the request is invalid or/and incorrect.
 ///
 /// # Example
 ///
 /// Follow [`Evds`](crate::common::Evds) for full and detailed implementation of **evds** argument.
 ///
 /// ```
-/// #   use tcmb_evds::date::{Date, DatePreference};     
-/// #   use tcmb_evds::common::{Evds, ApiKey, ReturnFormat};     
+/// #   use tcmb_evds::date::{Date, DatePreference};
+/// #   use tcmb_evds::common::{Evds, ApiKey, ReturnFormat};
 ///     use tcmb_evds::evds_basic;
-/// 
-/// 
+///
+///
 ///     // declaration and assignment of required argument.
-///     // another possible data series = "TP.DK.USD.A-TP.DK.USD.S-TP.DK.GBP.A-TP.DK.GBP.S" 
+///     // another possible data series = "TP.DK.USD.A-TP.DK.USD.S-TP.DK.GBP.A-TP.DK.GBP.S"
 ///     let data_series = "TP.DK.USD.A";
 /// #
 /// #   let date_result = Date::from("13-12-2011");
-/// #   let date = 
-/// #       if let Ok(date) = date_result { date } 
-/// #       else { return };     
+/// #   let date =
+/// #       if let Ok(date) = date_result { date }
+/// #       else { return };
 /// #   let date_preference = DatePreference::Single(date);
-/// #   
-/// #   let api_key = 
-/// #       if let Ok(api_key) = ApiKey::from("users_api_key") { api_key } 
-/// #       else { return }; 
-/// #   
+/// #
+/// #   let api_key =
+/// #       if let Ok(api_key) = ApiKey::from("users_api_key") { api_key }
+/// #       else { return };
+/// #
 /// #   let evds = Evds::from(api_key, ReturnFormat::Xml);
-/// 
+///
 ///
 ///     // requesting data section.
 ///     let result = evds_basic::get_data(data_series, &date_preference, &evds);
 ///
-///     
+///
 ///     // error handling or getting the data.
 ///     let data = match result {
-///         Err(error) => { 
+///         Err(error) => {
 ///             println!("{}", error.to_string());
 ///             return;
 ///         },
-///         Ok(data) => data,     
+///         Ok(data) => data,
 ///     };
 /// ```
 pub fn get_data<'a>(
-    data_series: &str, 
-    date_preference: &date::DatePreference, 
+    data_series: &str,
+    date_preference: &date::DatePreference,
     evds: &common::Evds,
 ) -> Result<String, ReturnError> {
 
@@ -72,15 +72,15 @@ pub fn get_data<'a>(
 
     basic::check_emptiness(data_series)?;
 
-    let url = 
+    let url =
     format!(
-        "https://evds2.tcmb.gov.tr/service/evds/series={}&{}&{}&{}", 
-        data_series, 
+        "https://evds2.tcmb.gov.tr/service/evds/series={}&{}&{}&{}",
+        data_series,
         dates_as_url,
-        return_format_as_url, 
+        return_format_as_url,
         api_key_as_url,
     );
-    
+
     basic::make_request(&url, basic::Function::OneOfOtherFunctions)
 }
 
@@ -92,38 +92,38 @@ pub fn get_data<'a>(
 ///
 /// [`Date`](struct@crate::date::Date) and [`DateRange`](struct@crate::date::DateRange) are both acceptable
 /// for this function.
-/// 
+///
 /// *User is responsible to supply correct and valid* **data group** *argument for this function.*
 ///
 /// # Error
 ///
 /// This function returns an error if some of given parameters is empty, the internet connection is poor or/and
-/// the format of the request is invalid or/and incorrect. 
+/// the format of the request is invalid or/and incorrect.
 ///
 /// # Example
 ///
-/// Follow [`DatePreference`](crate::date::DatePreference) and [`Evds`](crate::common::Evds) for full and detailed 
+/// Follow [`DatePreference`](crate::date::DatePreference) and [`Evds`](crate::common::Evds) for full and detailed
 /// implementation of **date_preference** and **evds** arguments respectively.
 ///
 /// ```
-/// #   use tcmb_evds::date::{DateRange, DatePreference};     
-/// #   use tcmb_evds::common::{Evds, ApiKey, ReturnFormat};     
+/// #   use tcmb_evds::date::{DateRange, DatePreference};
+/// #   use tcmb_evds::common::{Evds, ApiKey, ReturnFormat};
 ///     use tcmb_evds::evds_basic;
-/// 
-/// 
+///
+///
 ///     // declaration and assignment of required argument.
 ///     let data_group = "bie_yssk";
 /// #
 /// #   let date_range_result = DateRange::from("13-12-2011", "12-11-2015");
-/// #   let date_range = 
-/// #       if let Ok(date_range) = date_range_result { date_range } 
-/// #       else { return };     
+/// #   let date_range =
+/// #       if let Ok(date_range) = date_range_result { date_range }
+/// #       else { return };
 /// #   let date_preference = DatePreference::Multiple(date_range);
-/// #    
-/// #   let api_key = 
-/// #       if let Ok(api_key) = ApiKey::from("users_api_key") { api_key } 
-/// #       else { return }; 
-/// #   
+/// #
+/// #   let api_key =
+/// #       if let Ok(api_key) = ApiKey::from("users_api_key") { api_key }
+/// #       else { return };
+/// #
 /// #   let evds = Evds::from(api_key, ReturnFormat::Xml);
 /// #
 ///
@@ -131,19 +131,19 @@ pub fn get_data<'a>(
 ///     // requesting data section.
 ///     let result = evds_basic::get_data_group(data_group, &date_preference, &evds);
 ///
-///     
+///
 ///     // error handling or getting the data.
 ///     let data = match result {
-///         Err(error) => { 
+///         Err(error) => {
 ///             println!("{}", error.to_string());
 ///             return;
 ///         },
-///         Ok(data) => data,     
+///         Ok(data) => data,
 ///     };
 /// ```
 pub fn get_data_group(
-    data_group: &str, 
-    date_preference: &date::DatePreference, 
+    data_group: &str,
+    date_preference: &date::DatePreference,
     evds: &common::Evds,
 ) -> Result<String, ReturnError> {
 
@@ -153,12 +153,12 @@ pub fn get_data_group(
 
     basic::check_emptiness(data_group)?;
 
-    let url = 
+    let url =
     format!(
-        "https://evds2.tcmb.gov.tr/service/evds/datagroup={}&{}&{}&{}", 
-        data_group, 
+        "https://evds2.tcmb.gov.tr/service/evds/datagroup={}&{}&{}&{}",
+        data_group,
         dates_as_url,
-        return_format_as_url, 
+        return_format_as_url,
         api_key_as_url,
     );
 
@@ -172,34 +172,34 @@ pub fn get_data_group(
 /// # Error
 ///
 /// This function returns an error if some of given parameters is empty, the internet connection is poor or/and
-/// the format of the request is invalid or/and incorrect. 
+/// the format of the request is invalid or/and incorrect.
 ///
 /// # Example
 ///
 /// Follow [`Evds`](crate::common::Evds) for full and detailed implementation of **evds** argument.
 ///
 /// ```
-/// #   use tcmb_evds::common::{Evds, ApiKey, ReturnFormat};     
+/// #   use tcmb_evds::common::{Evds, ApiKey, ReturnFormat};
 ///     use tcmb_evds::evds_basic;
-/// 
-/// 
-/// #   let api_key = 
-/// #       if let Ok(api_key) = ApiKey::from("users_api_key") { api_key } 
-/// #       else { return }; 
-/// #   
+///
+///
+/// #   let api_key =
+/// #       if let Ok(api_key) = ApiKey::from("users_api_key") { api_key }
+/// #       else { return };
+/// #
 /// #   let evds = Evds::from(api_key, ReturnFormat::Xml);
 /// #
 ///     // requesting data section.
 ///     let result = evds_basic::get_categories(&evds);
-///     
-///     
+///
+///
 ///     // error handling or getting the data.
 ///     let data = match result {
-///         Err(error) => { 
+///         Err(error) => {
 ///             println!("{}", error.to_string());
 ///             return;
 ///         },
-///         Ok(data) => data,     
+///         Ok(data) => data,
 ///     };
 /// ```
 pub fn get_categories(evds: &common::Evds) -> Result<String, ReturnError> {
@@ -207,7 +207,7 @@ pub fn get_categories(evds: &common::Evds) -> Result<String, ReturnError> {
     let return_format_as_url = evds.get_return_format_as_url();
     let api_key_as_url = evds.get_api_key_as_url();
 
-    let url = 
+    let url =
     format!{
         "https://evds2.tcmb.gov.tr/service/evds/categories/{}&{}",
         api_key_as_url,
@@ -217,10 +217,10 @@ pub fn get_categories(evds: &common::Evds) -> Result<String, ReturnError> {
     basic::make_request(&url, basic::Function::OneOfOtherFunctions)
 }
 
-/// returns required data groups.
+/// returns specified data groups.
 ///
 /// Data should be detached from the result to have information about data groups.
-/// 
+///
 /// It is because code parameter can be a string parameter that this parameter is required as string slice.
 ///
 /// *Users are responsible to supply correct and valid* **mode** *and* **code** *arguments for this function.*
@@ -228,21 +228,21 @@ pub fn get_categories(evds: &common::Evds) -> Result<String, ReturnError> {
 /// # Error
 ///
 /// This function returns an error if some of given parameters is empty, the internet connection is poor or/and
-/// the format of the request is invalid or/and incorrect. 
+/// the format of the request is invalid or/and incorrect.
 ///
 /// # Example
 ///
 /// Follow [`Evds`](crate::common::Evds) for full and detailed implementation of **evds** argument.
 ///
 /// ```
-/// #   use tcmb_evds::common::{Evds, ApiKey, ReturnFormat};     
+/// #   use tcmb_evds::common::{Evds, ApiKey, ReturnFormat};
 ///     use tcmb_evds::evds_basic;
-/// 
-///     
-/// #   let api_key = 
-/// #       if let Ok(api_key) = ApiKey::from("users_api_key") { api_key } 
-/// #       else { return }; 
-/// #   
+///
+///
+/// #   let api_key =
+/// #       if let Ok(api_key) = ApiKey::from("users_api_key") { api_key }
+/// #       else { return };
+/// #
 /// #   let evds = Evds::from(api_key, ReturnFormat::Xml);
 /// #
 ///     // declaration and assignment of the required arguments.
@@ -252,35 +252,35 @@ pub fn get_categories(evds: &common::Evds) -> Result<String, ReturnError> {
 ///
 ///     // requesting data section.
 ///     let result = evds_basic::get_advanced_data_group(mode, code, &evds);
-///     
-///     
+///
+///
 ///     // error handling or getting the data.
 ///     let data = match result {
-///         Err(error) => { 
+///         Err(error) => {
 ///             println!("{}", error.to_string());
 ///             return;
 ///         },
-///         Ok(data) => data,     
+///         Ok(data) => data,
 ///     };
 /// ```
 pub fn get_advanced_data_group(
-    mode: u32, 
-    code: &str, 
+    mode: u32,
+    code: &str,
     evds: &common::Evds
 ) -> Result<String, ReturnError> {
 
     let return_format_as_url = evds.get_return_format_as_url();
     let api_key_as_url = evds.get_api_key_as_url();
-    
+
     basic::check_emptiness(code)?;
-    
-    let url = 
+
+    let url =
     format!(
-        "https://evds2.tcmb.gov.tr/service/evds/datagroups/{}&mode={}&code={}&{}", 
+        "https://evds2.tcmb.gov.tr/service/evds/datagroups/{}&mode={}&code={}&{}",
         api_key_as_url,
-        mode, 
-        code, 
-        return_format_as_url, 
+        mode,
+        code,
+        return_format_as_url,
     );
 
     basic::make_request(&url, basic::Function::OneOfOtherFunctions)
@@ -289,7 +289,7 @@ pub fn get_advanced_data_group(
 /// returns all usable series list.
 ///
 /// Data should be detached from the result to have the list of series.
-/// 
+///
 /// It is because code parameter can be a string parameter that this parameter is required as string slice.
 ///
 /// *User is responsible to supply correct and valid* **code** *argument for this function.*
@@ -297,21 +297,21 @@ pub fn get_advanced_data_group(
 /// # Error
 ///
 /// This function returns an error if some of given parameters is empty, the internet connection is poor or/and
-/// the format of the request is invalid or/and incorrect. 
+/// the format of the request is invalid or/and incorrect.
 ///
 /// # Example
 ///
 /// Follow [`Evds`](crate::common::Evds) for full and detailed implementation of **evds** argument.
 ///
 /// ```
-/// #   use tcmb_evds::common::{Evds, ApiKey, ReturnFormat};     
+/// #   use tcmb_evds::common::{Evds, ApiKey, ReturnFormat};
 ///     use tcmb_evds::evds_basic;
-/// 
-///     
-/// #   let api_key = 
-/// #       if let Ok(api_key) = ApiKey::from("users_api_key".to_string()) { api_key } 
-/// #       else { return }; 
-/// #   
+///
+///
+/// #   let api_key =
+/// #       if let Ok(api_key) = ApiKey::from("users_api_key".to_string()) { api_key }
+/// #       else { return };
+/// #
 /// #   let evds = Evds::from(api_key, ReturnFormat::Csv);
 /// #
 ///     // required data declaration and assignment.
@@ -320,33 +320,33 @@ pub fn get_advanced_data_group(
 ///
 ///     // requesting data section.
 ///     let result = evds_basic::get_series_list(code, &evds);
-///     
-///     
+///
+///
 ///     // error handling or getting the data.
 ///     let data = match result {
-///         Err(error) => { 
+///         Err(error) => {
 ///             println!("{}", error.to_string());
 ///             return;
 ///         },
-///         Ok(data) => data,     
+///         Ok(data) => data,
 ///     };
 /// ```
 pub fn get_series_list(
-    code: &str, 
+    code: &str,
     evds: &common::Evds
 ) -> Result<String, ReturnError> {
-    
+
     if code.is_empty() { return Err(ReturnError::EmptyParameter); }
 
     let return_format_as_url = evds.get_return_format_as_url();
     let api_key_as_url = evds.get_api_key_as_url();
 
-    let url = 
+    let url =
     format!(
         "https://evds2.tcmb.gov.tr/service/evds/serieList/{}&{}&code={}",
         api_key_as_url,
-        return_format_as_url, 
-        code, 
+        return_format_as_url,
+        code,
     );
 
     basic::make_request(&url, basic::Function::GetSeriesList)
--- src/lib.rs
+++ src/lib.rs
@@ -3,31 +3,31 @@
 //! This crate provides two main separate mechanisms for acquiring data from the database:
 //!
 //! - [`evds_basic`](crate::evds_basic) includes functions making most of the web service operations except
-//! currency operations with frequency formulas. 
-//! - [`evds_currency`](crate::evds_currency) includes structure-based and implemented methods that make totally 
+//! currency operations with frequency formulas.
+//! - [`evds_currency`](crate::evds_currency) includes structure-based and implemented methods that make totally
 //! currency operations.
 //!
-//! Useful functions of [`evds_basic`](crate::evds_basic) and [`evds_currency`](crate::evds_currency) 
-//! require a number of common elements checking the validity of given inputs and creating appropriate requests formats 
+//! Useful functions of [`evds_basic`](crate::evds_basic) and [`evds_currency`](crate::evds_currency)
+//! require a number of common elements checking the validity of given inputs and creating appropriate requests formats
 //! for the database. These mentioned elements are included in [`common`], [`date`] and [`error`] modules.
 //!
-//! [`ApiKey`](struct@crate::common::ApiKey), which is a structure in [`common`], gives validity to all functions 
+//! [`ApiKey`](struct@crate::common::ApiKey), which is a structure in [`common`], gives validity to all functions
 //! playing role to make requests for EVDS web services. Therefore, users need to utilize their keys to work with EVDS.
 //!
-//! The functions making most of EVDS web service operations are given inside the [`evds_basic`](crate::evds_basic) 
-//! module except currency values with advanced features. In addition, [`evds_currency`](crate::evds_currency) has two 
-//! structures containing required currency requesting elements. It is because the number of the elements are more than 
-//! enough for using as parameter that these two struct supplies self methods to make only *currency* operations 
+//! The functions making most of EVDS web service operations are given inside the [`evds_basic`](crate::evds_basic)
+//! module except currency values with advanced features. In addition, [`evds_currency`](crate::evds_currency) has two
+//! structures containing required currency requesting elements. It is because the number of the elements are more than
+//! enough for using as parameter that these two struct supplies self methods to make only *currency* operations
 //! of EVDS web services.
 //!
 //! This crate provides `async_mode` and `sync_mode` as a feature. `async_mode` and `sync_mode` features
-//! should be used with an async and a sync programming respectively. Furthermore, the `async_mode` feature is enabled 
+//! should be used with an async and a sync programming respectively. Furthermore, the `async_mode` feature is enabled
 //! by default. One of these two features should be used to work with this crate.
 //!
 //! # Install
 //!
 //! Please, add **appropriate one** of the blow codes to your **Cargo.toml** to install the crate.
-//! 
+//!
 //!
 //! For *async_mode*, please add.
 //!
@@ -45,7 +45,7 @@
 //!
 //! # Usage
 //!
-//! For more and other function implementations and details, please go to [`evds_basic`](crate::evds_basic) module 
+//! For more and other function implementations and details, please go to [`evds_basic`](crate::evds_basic) module
 //! stage.
 //!
 //! ```
@@ -54,8 +54,8 @@
 //!     use tcmb_evds::*;
 //! #
 //! # fn main() -> Result<(), Box<dyn Error>> {
-//! 
-//! 
+//!
+//!
 //!     // assigning required arguments.
 //!     // another data series = "TP.DK.USD.A-TP.DK.USD.S-TP.DK.GBP.A-TP.DK.GBP.S"
 //!     let data_series = "TP.DK.USD.A";
@@ -74,17 +74,17 @@
 //! #   Ok(())
 //! # }
 //! ```
-//! For more and other function implementations and details, please go to [`evds_currency`](crate::evds_currency) module 
+//! For more and other function implementations and details, please go to [`evds_currency`](crate::evds_currency) module
 //! stage.
 //!
 //! ```
 //! # use std::error::Error;
 //!     use tcmb_evds::*;
-//! 
+//!
 //! # fn main() -> Result<(), Box<dyn Error>> {
-//! 
+//!
 //!     // common elements
-//!     let api_key = common::ApiKey::from("user_api_key".to_string())?; 
+//!     let api_key = common::ApiKey::from("user_api_key".to_string())?;
 //!     let return_format = common::ReturnFormat::Json;
 //!     let evds = common::Evds::from(api_key, return_format);
 //!
@@ -94,21 +94,21 @@
 //!
 //!     // currency series creation
 //!     let exchange_type = evds_currency::ExchangeType::new();
-//! 
+//!
 //!     let currency_code = evds_currency::CurrencyCode::Usd;
-//! 
+//!
 //!     // Ytl mode adds "YTL" to currency series, when it is true.
 //!     let ytl_mode = true;
-//!   
-//!     let currency_series = 
+//!
+//!     let currency_series =
 //!         evds_currency::CurrencySeries::from(
-//!             exchange_type, 
-//!             currency_code, 
-//!             date_preference, 
+//!             exchange_type,
+//!             currency_code,
+//!             date_preference,
 //!             ytl_mode
 //!         );
-//! 
-//! 
+//!
+//!
 //!     // get data operation based on created CurrencySeries.
 //!     let currency_data = currency_series.get_data(&evds)?;
 //!
@@ -121,10 +121,10 @@
 //! - evds_basic
 //!
 //!     - Provides **most of the EVDS web service operations** except currency value with frequency formulas
-//!     service which is called advanced currency operations in this crate. 
+//!     service which is called advanced currency operations in this crate.
 //!     - Users are responsible for ensuring validity of the given series and some data.
 //!     - Less reliable for the currency service operations.
-//! 
+//!
 //! - evds_currency
 //!
 //!     - Specified for operating **currency operations**.
@@ -133,13 +133,13 @@
 //!
 //! # User Guide
 //!
-//! First of all, please take a look at *[README.md](https://github.com/asari555/tcmb_evds/blob/master/README.md)* file 
+//! First of all, please take a look at *[README.md](https://github.com/asari555/tcmb_evds/blob/master/README.md)* file
 //! before using the crate.
 //!
-//! Additional resource for understanding some parameters, learning various data series and details of web services are 
+//! Additional resource for understanding some parameters, learning various data series and details of web services are
 //! mentioned in [`kullanim kilavuzu`] for Turkish and [`user guide`].
 //!
-//! 
+//!
 //! [`user guide`]: <https://evds2.tcmb.gov.tr/help/videos/EVDS_Web_Service_Usage_Guide.pdf>
 //! [`kullanim kilavuzu`]: <https://evds2.tcmb.gov.tr/help/videos/EVDS_Web_Servis_Kullanim_Kilavuzu.pdf>
 //!
@@ -155,15 +155,15 @@
 
 /// contains two main elements that are used in operations of
 /// [`evds_basic`](crate::evds_basic) and [`evds_currency`](crate::evds_currency).
-/// 
-/// [`ReturnFormat`](crate::common::ReturnFormat) specifies format of the database response and 
-/// [`ApiKey`](crate::common::ApiKey) is created to supply valid key to the functions making request operations. 
-/// [`Evds`](crate::common::Evds) becomes connection between the functions and both 
+///
+/// [`ReturnFormat`](crate::common::ReturnFormat) specifies format of the database response and
+/// [`ApiKey`](crate::common::ApiKey) is created to supply valid key to the functions making request operations.
+/// [`Evds`](crate::common::Evds) becomes connection between the functions and both
 /// [`ReturnFormat`](crate::common::ReturnFormat) and [`ApiKey`](crate::common::ApiKey) variables.
 ///
 /// # Usage
 ///
-/// The each element is explained and exampled in their definition parts. This usage illustrates 
+/// The each element is explained and exampled in their definition parts. This usage illustrates
 /// combined version of common elements on Evds structure to be used in related functions.
 ///
 /// ```
@@ -173,7 +173,7 @@
 /// # use tcmb_evds::error::ReturnError;
 /// #
 /// # fn main() -> Result<(), Box<dyn Error>> {
-/// 
+///
 ///     // Please use a valid key here.
 ///     let api_key = ApiKey::from("users_valid_key".to_string())?;
 ///
@@ -185,12 +185,12 @@
 /// # }
 /// ```
 pub mod common;
-/// contains date elements that are used in some functions of [`evds_basic`](crate::evds_basic) and 
+/// contains date elements that are used in some functions of [`evds_basic`](crate::evds_basic) and
 /// [`evds_currency`](crate::evds_currency).
 ///
-/// [`Date`](crate::date::Date) and [`DateRange`](crate::date::DateRange) are created to supply single date and multiple 
-/// dates called date range. [`DatePreference`](crate::date::DatePreference) includes [`Date`](crate::date::Date) or 
-/// [`DateRange`](crate::date::DateRange) and supplies date options to related functions of 
+/// [`Date`](crate::date::Date) and [`DateRange`](crate::date::DateRange) are created to supply single date and multiple
+/// dates called date range. [`DatePreference`](crate::date::DatePreference) includes [`Date`](crate::date::Date) or
+/// [`DateRange`](crate::date::DateRange) and supplies date options to related functions of
 /// [`evds_basic`](crate::evds_basic) and [`evds_currency`](crate::evds_currency).
 ///
 /// # Usage
@@ -209,8 +209,8 @@ pub mod common;
 ///     let date = Date::from("13-12-2011")?;
 ///
 ///     let date_preference = DatePreference::Single(date);
-///     
-///     
+///
+///
 ///     // Multiple dates example.
 ///     let date_range = DateRange::from("13-12-2011", "13-12-2020")?;
 ///
@@ -220,14 +220,14 @@ pub mod common;
 /// # }
 /// ```
 pub mod date;
-/// contains specified error options that are returned from the functions of 
+/// contains specified error options that are returned from the functions of
 /// [`evds_basic`](crate::evds_basic) and [`evds_currency`](crate::evds_currency) to illustrate why the error occurs.
 ///
-/// One of the [`ReturnError`](crate::error::ReturnError) options is returned when something goes wrong with requesting 
-/// data or giving parameter to the functions. Therefore, users are able to handle specified error types and to 
+/// One of the [`ReturnError`](crate::error::ReturnError) options is returned when something goes wrong with requesting
+/// data or giving parameter to the functions. Therefore, users are able to handle specified error types and to
 /// stringify them in a standard format.
 pub mod error;
-/// provides most of the EVDS web services except requesting advanced currency data that means currency data with 
+/// provides most of the EVDS web services except requesting advanced currency data that means currency data with
 /// frequency formulas.
 pub mod evds_basic;
 /// provides only currency operations with methods of [`CurrencySeries`] and [`MultipleCurrencySeries`].
@@ -235,16 +235,16 @@ pub mod evds_basic;
 /// This module built on two main structures and their methods operating currency services. The basic difference between
 /// these structures is number of currency types. [`CurrencySeries`](crate::evds_currency::CurrencySeries) serves
 /// methods using a currency type. In contrast, [`MultipleCurrencySeries`](crate::evds_currency::MultipleCurrencySeries)
-/// serves a method using more than a currency type to get currencies data.   
+/// serves a method using more than a currency type to get currencies data.
 ///
-/// `CurrencySeries` is composed of [`CurrencyCode`], `ExchangeType`, `DatePreference` and `ytl_mode` to supply 
-/// adequate information to make requesting data about **a currency** via related **operational methods**. 
+/// `CurrencySeries` is composed of [`CurrencyCode`], `ExchangeType`, `DatePreference` and `ytl_mode` to supply
+/// adequate information to make requesting data about **a currency** via related **operational methods**.
 ///
-/// `MultipleCurrencySeries` is composed of [`CurrencyCodes`], `ExchangeType`, `DatePreference` and `ytl_mode` to 
-/// supply adequate information to make requesting data about **given currencies** via **an operational method**. 
+/// `MultipleCurrencySeries` is composed of [`CurrencyCodes`], `ExchangeType`, `DatePreference` and `ytl_mode` to
+/// supply adequate information to make requesting data about **given currencies** via **an operational method**.
 ///
 /// ### Usage schematic of operational functions and hierarchy of this module
-/// 
+///
 /// - [`CurrencySeries`] -> [`get_data`] ( [`Evds`] ) or [`get_advanced_data`] ( [`Evds`], [`AdvancedProcesses`] )
 ///
 ///     > `CurrencySeries` requires below struct as a difference
@@ -266,7 +266,7 @@ pub mod evds_basic;
 /// - [`get_data`]
 ///
 /// - [`get_advanced_data`]
-/// 
+///
 /// - [`get_multiple_data`]
 ///
 /// To use the operational methods, the related structures for the required operation should be built firstly. Then,
